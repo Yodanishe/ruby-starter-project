99044b720028f7c199cd1cf27e1fada77c4deaa2
Vlad <Vlad.Lozovou@yandex.ru>
2021-10-12 14:47:47 +0700

    fix program.rb

--- lib/program.rb
+++ lib/program.rb
@@ -5,45 +5,48 @@ require_relative './enter'
 require_relative './terminal'
 
 class Program
-    def run
-        Terminal.say_hello
-        from = TemperatureFrom.from
-        while !TemperatureFrom.is_correct? from do 
-            Terminal.clear
-            from = TemperatureFrom.from
-        end
-
-        to = TemperatureTo.to
-        while !TemperatureTo.is_correct? from, to do
-            Terminal.clear
-            to = TemperatureTo.to
-        end
-
-        t1 = TemperatureEnter.enter
-        while !TemperatureEnter.is_float? t1 do
-            Terminal.clear
-            t1 = TemperatureEnter.enter
-        end
-        t1 = TemperatureEnter.to_float t1
-        t2 = convert from, to, t1
-
-        Terminal.print_answer from, to, t1, t2
+  def run
+    Terminal.say_hello
+    from = TemperatureFrom.from
+    until TemperatureFrom.is_correct? from
+      Terminal.clear
+      from = TemperatureFrom.from
     end
 
-    def convert from, to, t1
-        temperature = TemperatureTransfer.new
-        temperature.t = t1
-
-        case from
-        when "C"
-            if to == "F" then t2 = temperature.from_C_to_F!
-            else t2 = temperature.from_C_to_K! end
-        when "F"
-            if to == "C" then t2 = temperature.from_F_to_C!
-            else t2 = temperature.from_F_to_K! end
-        when "K"
-            if to == "C" then t2 = temperature.from_K_to_C!
-            else t2 = temperature.from_K_to_F! end
-        end
+    to = TemperatureTo.to
+    until TemperatureTo.is_correct? from, to
+      Terminal.clear
+      to = TemperatureTo.to
     end
+
+    t1 = TemperatureEnter.enter
+    until TemperatureEnter.is_float? t1
+      Terminal.clear
+      t1 = TemperatureEnter.enter
+    end
+    t1 = TemperatureEnter.to_float t1
+    t2 = convert from, to, t1
+
+    Terminal.print_answer from, to, t1, t2
+  end
+
+  def convert(from, to, t1)
+    temperature = TemperatureTransfer.new
+    temperature.t = t1
+
+    case from
+    when 'C'
+      t2 = if to == 'F' then temperature.from_C_to_F!
+           else
+             temperature.from_C_to_K!      end
+    when 'F'
+      t2 = if to == 'C' then temperature.from_F_to_C!
+           else
+             temperature.from_F_to_K!      end
+    when 'K'
+      t2 = if to == 'C' then temperature.from_K_to_C!
+           else
+             temperature.from_K_to_F!      end
+    end
+  end
 end
